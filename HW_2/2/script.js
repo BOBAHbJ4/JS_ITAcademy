236e9b37c4ed97e8d7365cd84e56b52840ae6532
BOBAHbl <spysystems.87@gmail.com>
2019-04-08 23:06:58 +0300

    HW

---
+++ HW_2/2/script.js
@@ -0,0 +1,14 @@
+'use strict'
+var a = Number(prompt('Enter the first number: ',1));
+var b = Number(prompt('Enter the second number: ',1));
+var sum = 0;
+if (a < b) {
+  for (var i = a; i <= b; i++) {
+    sum += i;
+  } alert (sum);
+} else if (a == b) {
+  sum = Number(a) + Number(b);
+  alert(sum);
+} else {
+  alert('First number cannot be greater than the second');
+}
