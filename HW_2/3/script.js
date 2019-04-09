55234ec7ae5dba469a53449e9e0517686770fb01
BOBAHbl <spysystems.87@gmail.com>
2019-04-09 19:46:14 +0300

    HW

--- HW_2/3/script.js
+++ HW_2/3/script.js
@@ -1,1 +1,12 @@
 'use strict'
+for (var x = 1; x <= 100; x++) {
+  if (x % 3 == 0 && x % 5 == 0) {
+    console.log('ThreeFive');
+  } else if (x % 3 == 0) {
+    console.log('Three');
+  } else if (x % 5 == 0) {
+    console.log('five');
+  } else {
+    console.log(x);
+  }
+}
---
+++ HW_2/5/script.js
@@ -0,0 +1,5 @@
+'use strict'
+var num = Number(prompt('How many people were there: ', 10));
+for (var x = 1; x <= a; x++) {
+  var age = Number(prompt('The age of the' + x + ' ' + 'visitor: ', 18));
+}
